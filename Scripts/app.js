cc5694e45fb3d98515ef39597fc4c3cad45ec153
secretspot92 <secretspot92@gmail.com>
2016-08-06 01:29:49 -0400

    Added nav bar and default map.

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,4 +6,16 @@ var App = {};
 
 App.config = {};
 App.config.api_key = "AIzaSyAGiGyrLbjIuXVdO6-cvuiO4ruwBYH_VXo";
+App.config.default_map = {
+    center: {
+        lat: 36.2414783,
+        lng: -113.754105
+    },
+    zoom: 4
+};
 
+
+function initMap() {
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, App.config.default_map);
+}
